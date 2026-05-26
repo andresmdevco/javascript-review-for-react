bbc13b7335a3f6cd53bb1a0d9ce2842d24c7b020
Andrés Muñoz <andres2078@gmail.com>
2026-05-25 23:05:59 -0500

    feat: add const variable example

---
+++ js/02.js
@@ -0,0 +1,9 @@
+const cliente = "Juan"
+// cliente = 30 (const - No se puede reasignar su valor)
+
+
+const precio = 1000 
+// cont precio (No pueden inicializarse sin valor)
+
+console.log(cliente)
+console.log(precio)
