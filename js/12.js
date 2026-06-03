d4ffd1d9710ebe8e3c65170167d3c5c9e752071b
Andrés Muñoz <andres2078@gmail.com>
2026-06-03 15:19:06 -0500

    feat: add array iteration with for loop examples

---
+++ js/12.js
@@ -0,0 +1,7 @@
+// Recorriendo arrays con for
+
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js', 'Nest.js', 'TypeScript']
+
+for(let i = 0; i < tecnologias.length; i++) {
+    console.log(tecnologias[i])
+}
