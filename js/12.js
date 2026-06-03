8b6da293e5bda822a41b81817a5f4b4bc3f35514
Andrés Muñoz <andres2078@gmail.com>
2026-06-03 15:32:55 -0500

    feat: add array iteration examples with forEach, map and for of

--- js/12.js
+++ js/12.js
@@ -2,6 +2,26 @@
 
 const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js', 'Nest.js', 'TypeScript']
 
-for(let i = 0; i < tecnologias.length; i++) {
-    console.log(tecnologias[i])
+// for(let i = 0; i < tecnologias.length; i++) {
+//     console.log(tecnologias[i])
+// }
+
+// forEach
+tecnologias.forEach(function(tech) {
+    console.log(tech)
+})
+
+// map (genera un arreglo nuevo)
+const arrayMap = tecnologias.map(function(tech) {
+    return tech
+})
+
+console.log(arrayMap)
+
+// for ... of
+for (let tech of tecnologias) {
+    console.log(tech)
 }
+
+
+
