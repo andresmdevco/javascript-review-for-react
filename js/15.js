82b5214512604d7bd2022a1622295e3d047ae2fe
Andrés Muñoz <andres2078@gmail.com>
2026-06-04 14:22:24 -0500

    feat: add arrow function example

---
+++ js/15.js
@@ -0,0 +1,12 @@
+// Funciones - Arrow Functions
+
+// const sumar = (numero1 = 0, numero2 = 0) => {
+//     console.log(numero1 + numero2) 
+// }
+
+const sumar = (numero1 = 0, numero2 = 0) => console.log(numero1 + numero2) 
+
+sumar(23, 15)
+sumar(300, 1)
+sumar(100)
+
