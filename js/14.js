8c4282e37af5c1fb3672e73434e39c300630c2fa
Andrés Muñoz <andres2078@gmail.com>
2026-06-04 14:45:59 -0500

    feat: add function return value examples

--- js/14.js
+++ js/14.js
@@ -4,6 +4,11 @@ const sumar = function(numero1 = 0, numero2 = 0) {
     console.log(numero1 + numero2) 
 }
 
+// function sumar(numero1 = 0, numero2 = 0) {
+//     console.log(numero1 + numero2) 
+// }
+
+
 sumar(23, 15)
 sumar(300, 1)
 sumar(100)
