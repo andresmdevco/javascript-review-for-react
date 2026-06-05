ac27819a0513826fbfa9bcbe1d93cf2588169c6a
Andrés Muñoz <andres2078@gmail.com>
2026-06-05 16:13:49 -0500

    feat: add named exports and import alias examples

--- js/24.js
+++ js/24.js
@@ -1,6 +1,6 @@
-import { sumar, restar } from './funciones.js';
+import { sumar as funcionSumar, restar } from './funciones.js';
 
-const resultado = sumar(20, 10)
+const resultado = funcionSumar(20, 10)
 const resultado2 = restar(25, 3)
 
 console.log(resultado)
--- js/funciones.js
+++ js/funciones.js
@@ -2,6 +2,10 @@ export function sumar(n1, n2) {
     return n1 + n2
 }
 
-export function restar(n1, n2) {
+function restar(n1, n2) {
     return n1 - n2
+}
+
+export {
+    restar
 }
