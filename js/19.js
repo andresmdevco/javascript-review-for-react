baba5681cc264cfa136769d3b09ddf2a150155f5
Andrés Muñoz <andres2078@gmail.com>
2026-06-04 23:47:37 -0500

    feat: add strict and loose equality comparison examples

---
+++ js/19.js
@@ -0,0 +1,23 @@
+const numero1 = 20
+const numero2 = "20"
+
+/*
+    ==  (Comparador no estricto)
+    === (Comparador estricto)
+*/
+
+// no estricto
+if(numero1 == numero2) {
+    console.log('Si, son iguales') // resultado
+} else {
+    console.log('No, no son iguales')
+}
+
+// estricto
+if(numero1 === numero2) {
+    console.log('Si, son iguales')
+} else {
+    console.log('No, no son iguales') // resultado
+}
+
+
