f63412eb813f120a604942afb91be2917cf2927c
Andrés Muñoz <andres2078@gmail.com>
2026-06-05 16:24:42 -0500

    feat: add default export and mixed module import examples

--- js/24.js
+++ js/24.js
@@ -1,7 +1,13 @@
-import { sumar as funcionSumar, restar } from './funciones.js';
+// import { sumar as funcionSumar, restar } from './funciones.js';
+import multiplica, { sumar, restar, division } from './funciones.js';
 
-const resultado = funcionSumar(20, 10)
+
+const resultado1 = sumar(20, 10)
 const resultado2 = restar(25, 3)
+const resultado3 = multiplica(4, 6)
+const resultado4 = division(46, 2)
 
-console.log(resultado)
+console.log(resultado1)
 console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
--- js/funciones.js
+++ js/funciones.js
@@ -1,11 +1,20 @@
-export function sumar(n1, n2) {
-    return n1 + n2
+export const sumar = (n1, n2) => n1 + n2
+
+export const restar = (n1, n2) =>  n1 - n2
+
+// export default function multiplicar(n1, n2) {
+//     return n1 * n2
+// }
+
+const multiplicar = (n1, n2) => n1 * n2
+
+export function division(n1, n2) {
+    return n1 / n2
 }
 
-function restar(n1, n2) {
-    return n1 - n2
-}
+export default multiplicar
 
-export {
-    restar
-}
+// export {
+//     sumar,
+//     restar
+// }
