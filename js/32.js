7d743575d80abcfb74830717bdc71aea799a97bd
Andrés Muñoz <andres2078@gmail.com>
2026-06-09 15:57:52 -0500

    feat: add form validation and dynamic alert messages

--- js/32.js
+++ js/32.js
@@ -7,11 +7,24 @@ formulario.addEventListener('submit', e => {
     const nombre = document.querySelector('#nombre').value
     const password = document.querySelector('#password').value
 
+    // Prevenir nuevas alertas
+    const alertaPrevia = document.querySelector('.alerta')
+    alertaPrevia?.remove()
+
+    const alerta = document.createElement('DIV')  
+    alerta.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'text-center', 'p-2', 'font-black')
+
     if (nombre === '' || password === '') {
-        console.log('Todos los campos son obligatorios')
+        alerta.textContent = 'Todos los campos son obligatorios...'
+        alerta.classList.add('bg-red-500')
     } else {
-        console.log('Todo bien, iniciando sesión...')
+        alerta.textContent = 'Todo bien, iniciando sesión...'
+        alerta.classList.add('bg-green-500')
     }
 
-    // console.log('Enviando formulario...')
+    formulario.appendChild(alerta)
+
+    setTimeout(() => {
+        alerta.remove()
+    }, 3000);
 })
